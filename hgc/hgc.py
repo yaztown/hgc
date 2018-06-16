8c3b02f9eb1039d37ea764f6464e15e7f5ecff42
Yaztown <yaztown@gmail.com>
2018-06-17 01:10:32 +0300

    Created a new repository for the HGC project

---
+++ hgc/hgc.py
@@ -0,0 +1,10 @@
+'''
+Created on Tuesday 12/06/2018
+
+@author: yaztown
+'''
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Animals',
+		version='0.1',
+		url='http://yaztown.com/',
+		author='Yaztown',
+		author_email='yaztown@gmail.com',
+		description='This is the Animals package',
+		long_description='Really, the funniest around.',
+		licence='MIT',
+		packages=['Animals'],
+		zip_safe=False)

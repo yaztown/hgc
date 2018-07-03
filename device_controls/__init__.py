b1d8ba7d8ce59a01807038194e55224371ce1e62
Yaztown <yaztown@gmail.com>
2018-07-03 17:14:49 +0300

    Added the imports in the __init__.py file
    - Added the concrete device classes to the module's main import.

---
+++ device_controls/__init__.py
@@ -0,0 +1,4 @@
+from device_controls.device_timing_controls import DeviceTimingControl
+from device_controls.device_sensor_controls import DeviceHumTempSensorControl
+
+__all__ = ['DeviceTimingControl', 'DeviceHumTempSensorControl']

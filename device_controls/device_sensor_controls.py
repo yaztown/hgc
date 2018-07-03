81bc094f243f04e169fdca9bb79372a9232e03a8
Yaztown <yaztown@gmail.com>
2018-07-03 20:16:13 +0300

    Modification: device_controls/device_sensor_controls.py
    Changed the __init__() param "sensor" to "sensor_name" which is a string
    name of the sensor thread which is matched against the list of current
    threads to be reference in the class.

--- device_controls/device_sensor_controls.py
+++ device_controls/device_sensor_controls.py
@@ -6,6 +6,7 @@ Created on Tuesday 03/07/2018
 
 from base_threads import BaseDeviceControl
 # from sensors import HumidityTemperatureSensor
+from threading import enumerate
 
 import weakref
 
@@ -14,12 +15,12 @@ class DeviceHumTempSensorControl(BaseDeviceControl):
     '''
     This class controls the device's On/Off using a sensor
     '''
-    def __init__(self, sensor=None,
+    def __init__(self, sensor_name=None,
                  threshold_humidity_upper=100, threshold_humidity_lower=0,
                  threshold_temp_upper=100, threshold_temp_lower=0,
                  *args, **kwargs):
         '''
-        sensor_list             : is a list of sensor threads
+        sensor_name             : is name of the sensor threads
         threshold_humidity_upper: is the humidity limit that will turn on the device 
         threshold_humidity_lower: is the humidity limit that will turn off the device
         threshold_temp_upper    : is the temperature limit that will turn on the device
@@ -27,9 +28,13 @@ class DeviceHumTempSensorControl(BaseDeviceControl):
         '''
         super().__init__(*args, **kwargs)
         
-        if sensor is None:
+        
+        if sensor_name is None:
             raise ValueError('Must provide a sensor object.')
-        self._sensor_ref = weakref.ref(sensor)
+        
+        thread_list = enumerate()
+        _sensor = [t for t in thread_list if t.name==sensor_name][0]
+        self._sensor_ref = weakref.ref(_sensor)
         
         self.threshold_hum_upper = threshold_humidity_upper
         self.threshold_hum_lower = threshold_humidity_lower

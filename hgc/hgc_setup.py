03d60ae8faad7801f5866e92afcfe247ec2ac03a
Yaztown <yaztown@gmail.com>
2019-01-23 15:18:09 +0300

    Modified the HGC Setup Object (hgc_setup.py)
    - All device controllers have two pins instead of one for p-p circuits
    - Changed the device_control_type for the exhaust fan controller to
      DeviceHumidityCompareControl
    - Added the intake fan controller which is a DeviceTempCompareControl
    - TODO: Think about replacing the dict objs with a meta class to point 
      to the right class definition.

--- hgc/hgc_setup.py
+++ hgc/hgc_setup.py
@@ -11,29 +11,37 @@ HGC_SETUP = {
     'sensors': [
         dict(name='hum_temp_dht_22_in',
              sensor_type='HumidityTemperatureSensor',
-             data_pin=4,
-             save_data=False),
+             data_pin=4,    # 7
+             save_data=True),
         dict(name='hum_temp_dht_22_out',
              sensor_type='HumidityTemperatureSensor',
-             data_pin=17,
-             save_data=False),
+             data_pin=17,   # 11
+             save_data=True),
     ],
+    # TODO: Think of replacing the dict objects into a class with a meta pointing to the write subclass
     'device_controls': [
         dict(name='light_control',
              device_control_type='DeviceTimingControl',
-             relay_pin=5,
+             relay_pin=[5, 6],  # 29, 31
              time_on=time(15),
              duration_on=timedelta(hours=18),
              cycles_per_day=1),
         dict(name='irrigation_control',
              device_control_type='DeviceTimingControl',
-             relay_pin=6,
+             relay_pin=[22, 27],    # 15, 13
              time_on=time(14,57),
              duration_on=timedelta(seconds=50),
              cycles_per_day=0.5),
         dict(name='exhaust_fan_control',
-             device_control_type='DeviceSensorsCompareControl',
-             relay_pin=7,
+             device_control_type='DeviceHumidityCompareControl',
+             relay_pin=[23, 24],    # 16, 18
+             sensor_in_name='hum_temp_dht_22_in',
+             sensor_out_name='hum_temp_dht_22_out',
+             threshold_humidity_upper=65,
+             ),
+        dict(name='intake_fan_control',
+             device_control_type='DeviceTempCompareControl',
+             relay_pin=[16, 26],    # 36, 37
              sensor_in_name='hum_temp_dht_22_in',
              sensor_out_name='hum_temp_dht_22_out',
              threshold_temp_upper=27,

ec87b055d87fb9758e2e5086be8a352dce23e406
Yaztown <yaztown@gmail.com>
2019-01-23 20:47:34 +0300

    Created a new module 'netserve' for webinterface
    - Added a very simple approch to create a HTTP server

---
+++ netserve/__init__.py
@@ -0,0 +1,3 @@
+from .MyHTTPServer import start_dev_server, SimpleAPIRequestHandler, ThreadingHTTPServer, JSONAPIRequestHandler
+
+__all__ = ['start_dev_server', 'SimpleAPIRequestHandler', 'ThreadingHTTPServer', 'JSONAPIRequestHandler']

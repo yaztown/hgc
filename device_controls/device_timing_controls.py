def1fa2d225c8c6ccc032c80bb8efbc345c2f26e
Yaztown <yaztown@gmail.com>
2019-02-19 18:44:13 +0300

    Modified DeviceTimingControl
    - Removed from_time_on_and_time_off classmethod
    - Changed __init__ parameters time_on & duration_on into dicts matching
      time & timedelta __init__ parameters.  This is necessary to enable
      loading json files

--- device_controls/device_timing_controls.py
+++ device_controls/device_timing_controls.py
@@ -16,8 +16,8 @@ class DeviceTimingControl(BaseDeviceControl):
     def __init__(self, time_on=None, duration_on=None, cycles_per_day=DEFAULT_CYCLES_PER_DAY,
                  *args, **kwargs):
         '''
-        time_on        : is a datetime.time() object indicating the time to turn on the device.
-        duration_on    : is a datetime.timedelta object indicating the period of time until
+        time_on        : is datetime.time() init parameters dictionary indicating the time to turn on the device.
+        duration_on    : is datetime.timedelta init parameters dictionary indicating the period of time until
                          turning the device off again.
         cycles_per_day : is a factor indicating how many times per day to repeat the off/on/off cycle.
                          e.g.: A value of 2 will repeat twice a day (12 hours per cycle)
@@ -27,11 +27,12 @@ class DeviceTimingControl(BaseDeviceControl):
         super().__init__(*args, **kwargs)
         
         if time_on is None or duration_on is None or cycles_per_day is None:
-            raise ValueError('One or more parameters is None.  Must ')
+            raise ValueError('One or more parameters is None. Must specify time_on and duration')
         
-        self.time_on = time_on
+        self.time_on = time(**time_on)
         self.cycles_per_day = cycles_per_day
         
+        duration_on = timedelta(**duration_on)
         self.cycle_total_duration = timedelta(days=1)/cycles_per_day
         
         if duration_on >= self.cycle_total_duration:
@@ -41,34 +42,34 @@ class DeviceTimingControl(BaseDeviceControl):
         self.duration_off = self.cycle_total_duration - duration_on
         
         today = datetime.now().date()
-        self.next_on = datetime.combine(today, time_on)
+        self.next_on = datetime.combine(today, self.time_on)
         self.next_off = self.next_on + self.duration_on
     
     
-    @classmethod
-    def from_time_on_and_time_off(cls, stime_on=None, stime_off=None, off_tomorrow=False, *args, **kwargs):
-        '''
-        stime_on : is a string object representing the Turn-On time with a 24-hour
-                  format "HH:MM:SS" e.g. "14:45:02", "15:56" or just "21".
-        stime_off: is a string object representing the Turn-Off time with a 24-hour
-                  format "HH:MM:SS" e.g. "15:55:15" and must be greater then time_on.
-        off_tomorrow: is a boolean indicating if the off time string happens the day after
-        '''
-        time_on = time( *map(int, stime_on.split(":")))
-        time_off = time( *map(int, stime_off.split(":")))
-        
-        if time_on >= time_off and not off_tomorrow:
-            raise ValueError('time_off must be greater than time_on')
-        
-        # The below varibles are datetime objects and are always moving
-        d_today = datetime.now().date()
-        d_tomorrow = d_today + timedelta(days=1)
-        
-        next_on = datetime.combine(d_today, time_on)
-        next_off = datetime.combine(d_tomorrow if off_tomorrow else d_today, time_off)
-        duration_on = next_off - next_on
-        
-        return cls(time_on=time_on, duration_on=duration_on, *args, **kwargs)
+#     @classmethod
+#     def from_time_on_and_time_off(cls, stime_on=None, stime_off=None, off_tomorrow=False, *args, **kwargs):
+#         '''
+#         stime_on : is a string object representing the Turn-On time with a 24-hour
+#                   format "HH:MM:SS" e.g. "14:45:02", "15:56" or just "21".
+#         stime_off: is a string object representing the Turn-Off time with a 24-hour
+#                   format "HH:MM:SS" e.g. "15:55:15" and must be greater then time_on.
+#         off_tomorrow: is a boolean indicating if the off time string happens the day after
+#         '''
+#         time_on = time( *map(int, stime_on.split(":")))
+#         time_off = time( *map(int, stime_off.split(":")))
+#         
+#         if time_on >= time_off and not off_tomorrow:
+#             raise ValueError('time_off must be greater than time_on')
+#         
+#         # The below varibles are datetime objects and are always moving
+#         d_today = datetime.now().date()
+#         d_tomorrow = d_today + timedelta(days=1)
+#         
+#         next_on = datetime.combine(d_today, time_on)
+#         next_off = datetime.combine(d_tomorrow if off_tomorrow else d_today, time_off)
+#         duration_on = next_off - next_on
+#         
+#         return cls(time_on=stime_on, duration_on=duration_on.seconds, *args, **kwargs)
     
     
     def setup_next_cycle(self):

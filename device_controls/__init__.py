058842e5b46f88e870bf556efc76c0cfb1e00ea7
Yaztown <yaztown@gmail.com>
2019-02-19 07:43:23 +0300

    Modified References to module 'device_controls'
    - Added DeviceHumidityCompareControl to device_controls.__init__
    - Added DeviceTempCompareControl to device_controls.__init__

--- device_controls/__init__.py
+++ device_controls/__init__.py
@@ -1,4 +1,8 @@
 from device_controls.device_timing_controls import DeviceTimingControl
-from device_controls.device_sensor_controls import DeviceHumTempSensorControl, DeviceSensorsCompareControl
+from device_controls.device_sensor_controls import \
+DeviceHumTempSensorControl, DeviceSensorsCompareControl, DeviceHumidityCompareControl, DeviceTempCompareControl
 
-__all__ = ['DeviceTimingControl', 'DeviceHumTempSensorControl', 'DeviceSensorsCompareControl']
+__all__ = [
+    'DeviceTimingControl', 'DeviceHumTempSensorControl', 'DeviceSensorsCompareControl',
+    'DeviceHumidityCompareControl', 'DeviceTempCompareControl'
+    ]

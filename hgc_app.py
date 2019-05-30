cc13f9289e219fadc8120312cb5da9b5eba58cc6
Yaztown <yaztown@gmail.com>
2019-05-31 01:01:09 +0300

    Modified the main app with:
    - Using hgc_logging
    - Added the registeration for the TERM and INT signals to graceful
      shutdown
    - Modified the loop logic and exception handling in the main while loop

--- hgc_app.py
+++ hgc_app.py
@@ -5,32 +5,36 @@ Created on Tuesday 03/07/2018
 '''
 
 from time import sleep
-from hgc import MainLoop, load_settings_from_file, HGC_SETTINGS, HGC_SETTINGS_TEST
+from hgc import MainLoop, load_settings_from_file #, HGC_SETTINGS, HGC_SETTINGS_TEST
+from base_threads import register_exit_signal_handler, ThreadExitException
 
-import logging
-#TODO: Centralize the logging elsewhere
-import os
-path = os.path.abspath(__file__)
-dir_path = os.path.dirname(path)
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(threadName)-20s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M:%S',
-                    filename=os.path.join(dir_path, 'log', 'hgc_app.log.txt'),
-                    filemode='w'
-                    )
+import hgc_logging
+logger = hgc_logging.get_logger()
 
 def main():
     settings = load_settings_from_file('hgc_settings_minimal.json')
     main_loop = MainLoop(setup_object=settings, name='main_loop', loop_sleep_time=1)
-    logging.debug('Starting main_loop')
+    logger.debug('Starting main_loop')
+    register_exit_signal_handler()
     main_loop.start()
-    
-    _exit_loop = False
-    
-    while not _exit_loop:
-        if not main_loop.get_status():
-            raise Exception()
-        sleep(2)
+    try:
+        # Keep the main thread running, otherwise signals are ignored.
+        while True:
+#             main_loop.get_status()
+            sleep(0.5)
+ 
+    except ThreadExitException:
+        # Terminate the running threads.
+        # Set the shutdown flag on each thread to trigger a clean shutdown of each thread.
+        main_loop.stop()
+        # Wait for the threads to close...
+        main_loop.join()
+    logger.debug('Exiting...')
+#     
+#     while not _exit_loop:
+#         if not main_loop.get_status():
+#             raise Exception()
+#         sleep(2)
 
 if __name__ == '__main__':
     main()

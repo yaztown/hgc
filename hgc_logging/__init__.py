765481e572826830f65a5cfce72cbf5c0f89f29b
Yaztown <yaztown@gmail.com>
2019-05-31 21:36:09 +0300

    Added the default_log_dirname to the hgc_logging package __all__
    - Removed the log_dir from the settings file

--- hgc_logging/__init__.py
+++ hgc_logging/__init__.py
@@ -3,6 +3,6 @@ Created on Monday 11/02/2019
 
 @author: yaztown
 '''
-from .logger_helper import get_logger
+from .logger_helper import get_logger, default_log_dirname
 
-__all__ = ['get_logger']
+__all__ = ['get_logger', 'default_log_dirname']

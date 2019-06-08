9182b77909468c4eee4492ffc0c732b38bebba44
Yaztown <yaztown@gmail.com>
2019-06-08 03:33:45 +0300

    renamed base_device_control to base_controller

--- base_threads/base_controller.py
+++ base_threads/base_controller.py
@@ -8,10 +8,10 @@ from .base_threads import BaseThread
 
 from pin_out import MyGPIO
 
-class BaseDeviceControl(BaseThread):
+class BaseController(BaseThread):
     '''
-    BaseDeviceControl class is the base of all the device control classes such as
-    the TimedDeviceControl class and sensor enabled device controls.
+    BaseController class is the base of all controller classes such as
+    the TimedController class and sensor powered controllers.
     '''
     
     def __init__(self, relay_pin=None, manual_control=False,
@@ -25,7 +25,7 @@ class BaseDeviceControl(BaseThread):
         gpio.setup(self.relay_pin, gpio.OUT)
         self.manual_control = manual_control
         
-        self._device_on = None     # Flag for Device power status
+        self._controller_on = None     # Flag for controller power status
     
     def __loop__(self):
         if not self.manual_control:
@@ -41,14 +41,14 @@ class BaseDeviceControl(BaseThread):
     
     def set_manual(self):
         '''
-        This method will set the operation of the device to manual as opposed to automatic
+        This method will set the operation of the controller to manual as opposed to automatic
         (see set_auto() for the other type of operation)
         '''
         self.manual_control = True
     
     def set_automatic(self):
         '''
-        This method will set the operation of the device to automatic as opposed to manual
+        This method will set the operation of the controller to automatic as opposed to manual
         (see set_manual() for the other type of operation)
         '''
         self.manual_control = False
@@ -56,7 +56,7 @@ class BaseDeviceControl(BaseThread):
     
     def _on_(self):
         '''
-        This is the method the does the actual turning on of the device
+        This is the method that does the actual turning on of the controller
         by switching the relay pin to logic high.
         
         *** Note: If needed, subclasses should override this method not self.turn_on()
@@ -66,27 +66,27 @@ class BaseDeviceControl(BaseThread):
     def _off_(self):
         
         '''
-        This is the method the does the actual turning off of the device
+        This is the method the does the actual turning off of the controller
         by switching the relay pin to logic low.
         
         *** Note: If needed, subclasses should override this method not self.turn_off()
         '''
         raise NotImplementedError('TODO: must be implemented by assigning the correct logic on the relay_pin')
     
-    # Device main On and Off methods
+    # Controller main On and Off methods
     def turn_on(self):
         '''
-        This method is called to turn on the device.
+        This method is called to turn on the controller.
         '''
         self._on_()
-        self._device_on = True
+        self._controller_on = True
     
     def turn_off(self):
         '''
-        This method is called to turn off the device.
+        This method is called to turn off the controller.
         '''
         self._off_()
-        self._device_on = False
+        self._controller_on = False
     
     @property
     def _serialized_(self):
@@ -94,6 +94,6 @@ class BaseDeviceControl(BaseThread):
         serialized.update({
             'relay_pin': self.relay_pin,
             'manual_control': self.manual_control,
-            '_device_on': self._device_on,
+            '_controller_on': self._controller_on,
         })
         return serialized

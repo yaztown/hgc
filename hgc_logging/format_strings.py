47d58004539c35593a365d6bc3bc9e4736d86779
Yaztown <yaztown@gmail.com>
2019-06-08 04:18:51 +0300

    modified the log format

--- hgc_logging/format_strings.py
+++ hgc_logging/format_strings.py
@@ -19,7 +19,7 @@ fmt_info = '''----------
 fmt_date_small = '%m-%d %H:%M:%S'
 
 
-fmt_simple_debug = '%(asctime)s %(levelname)-8s %(threadName)-20s %(module)s -> %(funcName)s @ %(lineno)d: %(message)s'
+fmt_simple_debug = '%(asctime)s %(levelname)-8s %(threadName)-20s -> %(funcName)s @ %(lineno)d: %(message)s'
 
-fmt_simpole_info = '%(asctime)s %(levelname)-8s %(threadName)s: %(message)s'
+fmt_simple_info = '%(asctime)s %(levelname)-8s %(threadName)s: %(message)s'
 
